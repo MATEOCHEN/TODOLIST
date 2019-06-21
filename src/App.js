221d2d3c811122ecc5f6d59d604180b627de7194
孟宣 陳 <stezen129@hotmail.com>
2019-06-21 21:41:43 +0800

    add the new style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import List from './List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = styled.div`
+  width:100&;
+  height: 100px;
+  background-color: skyblue;
+  text-align:center;
+`
+const Body = styled.div`
+  margin-top:50px;
+  text-align:center;
+`
+const Title = styled.p`
+  font-size:50px;
+  font-family:"微軟正黑體";
+`
+const Input = styled.input`
+  margin-left:25px;
+  border-radius:10px;
+  font-size:48px;
+  font-family:"微軟正黑體";
+  width: 55%;
+`
+const Label = styled.label`
+  font-size:48px;
+  width: 50px;
+`
+const AddButton = styled.button`
+  position:relative;
+  font-size:16px;
+  top:40px;
+  right:50px;
+  cursor: pointer;
+`
+
+const ListBlock = styled.div`
+  display:flex;
+  flex-direction: row;
+  flex-wrap:wrap;
+`
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      enentValue:"",
+      eventArray:[],
+      eventStatus:[]
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
+    this.handleCancelButtonClick = this.handleCancelButtonClick.bind(this);
+    this.handleFinishButtonClick = this.handleFinishButtonClick.bind(this);
+  };
+
+  handleInputChange(e) {
+    this.setState({enentValue:e.target.value})
+  }
+
+  handleAddButtonClick(){
+    let newEventArray = Object.assign([],this.state.eventArray);
+    let newEventStatus = Object.assign([],this.state.eventStatus);
+    newEventArray.push(this.state.enentValue);
+    newEventStatus.push(false);
+    this.setState({eventArray:newEventArray,eventStatus:newEventStatus});
+  }
+
+  handleFinishButtonClick(index){
+    let newEventStatus = Object.assign([],this.state.eventStatus);
+    newEventStatus[index] = true;
+    this.setState({eventStatus:newEventStatus});
+  }
+
+  handleCancelButtonClick(index){
+    let existedEventArray = Object.assign([],this.state.eventArray);
+    let existedEventStatus = Object.assign([],this.state.eventStatus);
+    existedEventArray.splice(index,1);
+    existedEventStatus.splice(index,1);
+    var newArray = existedEventArray;
+    var newStatus = existedEventStatus;
+    console.log(newArray);
+    this.setState({eventArray:newArray,eventStatus:newStatus});
+  }
+  render() {
+    const todoList = this.state.eventArray;
+    return (
+      <>
+      <Header>
+        <Title>todo-list</Title>
+      </Header>
+      <Body>
+        <Label htmlFor="eventInput">TODO:</Label>
+        <Input id="eventInput" type="text" onChange={this.handleInputChange} placeholder="Please input the event"/>
+        <AddButton onClick={this.handleAddButtonClick}>ADD</AddButton>        
+      </Body>
+      <ListBlock>
+      {todoList.map((item,index)=>(
+        <List 
+        status={this.state.eventStatus[index]} 
+        key={index} 
+        FinishClick={this.handleFinishButtonClick}
+        CancelClick={this.handleCancelButtonClick} 
+        event={item} 
+        index={index}
+        />))}
+      </ListBlock>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Block = styled.div`
+  display:block;
+  width: 20%;
+  height: 300px;
+  border-radius:20px;
+  background-color: #FFC0CB;
+  margin:40px 0px 0px 25px;
+  text-align:center;
+  word-wrap: break-word;
+  opacity: ${props=> props.status ? 0.5:1};
+`
+
+const Title = styled.p`
+    font-weight:bold;
+    font-size:32px;
+    color: deeppink;
+`
+
+const Content = styled.p`
+    font-size:24px;
+    font-weight:bold;
+    color:black;
+    font-family:"微軟正黑體";
+    
+`
+const ButtonBlock = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-evenly;
+`
+const Button = styled.input`
+    background-color: cornflowerblue;
+    position:relative;
+    top:100px;
+    border-radius:5px;
+    text-align:center;
+    visibility:${props=>props.status?"hidden":"visible"}
+`
+ class List extends React.Component{
+    render(){
+        const event = this.props.event;
+        const index = this.props.index;
+        const CancelFunction = this.props.CancelClick;
+        const FinishFunction = this.props.FinishClick;
+        const status = this.props.status;
+        return(
+            <Block status={status}>
+                <Title>Task{index + 1}</Title>
+                    <Content>{event}</Content>
+                    <ButtonBlock>
+                        <Button status={status} type="button" onClick={()=>FinishFunction(index)} value="Finish"/>
+                        <Button status={status} type="button" onClick={()=>CancelFunction(index)} value="Cancel"/>
+                    </ButtonBlock>
+            </Block>
+        );
+    }
+}
+export default List;
